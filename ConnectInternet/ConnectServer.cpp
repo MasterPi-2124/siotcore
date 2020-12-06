e513ea7e7773fcd5dcc84538c98f4f90635e7caf
linhit <donglinhit.hust@gmail.com>
2020-12-06 21:05:32 +0700

    base debug

--- ConnectInternet/ConnectServer.cpp
+++ ConnectInternet/ConnectServer.cpp
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#define CONNECT_SERVER_DEBUG
+#include "Debug.h"
 #include "ConnectServer.h"
 #include "ConnectInternet.h" 
 #include "RestClient.h"
@@ -8,8 +8,15 @@
 #define STATUS_CODE_OKE 200
 
 void update_progress(int cur, int total) {
-  Serial.printf("Update %d of %d bytes...\n", cur, total);
+  Debug::LOG_PROGRESS_UPDATE(cur,total);
 }
+void update_finished() {
+  Debug::LOG_TO_SCREEN(0,10,"Firmeware update successfully!");
+}
+void update_error(int err) {
+  Debug::LOG_TO_SCREEN(0,10,"Firmeware update error!");
+}
+
 
 enum CONNECT_SERVER{
     CONNECT_FAIL,
@@ -26,16 +33,17 @@ bool ConnectServer::isServerConnected(){
     while(!cni.isConnected()){
         // statement
         cni.connect();
-        DEBUG_CONNECT_PRINT("Waiting for connect...");
+        Debug::LOG_TO_SCREEN(0,0,"Waiting for connect...");
         delay(100);
     }
-    DEBUG_CONNECT_PRINT("Start connect...");
+    Debug::LOG_TO_SCREEN(0,0,"Start connect...");
     String response="";
     int status = client.get(URI_CHECK_COMMUNICATION, &response);
     if(status==200 && response.equalsIgnoreCase("CONNECTED")){
         return CONNECT_SUCCESSFULL;
     }
-    DEBUG_CONNECT_PRINT(status+"  "+response);
+    String txt =status+"  "+response;
+    Debug::LOG_TO_SCREEN(0,0,txt);
     return CONNECT_FAIL;
     
 }
@@ -48,18 +56,21 @@ int ConnectServer::getVersion(){
   return status;
 }
 int ConnectServer::updateFirmware(void){
+  
+  ESPhttpUpdate.onEnd(update_finished);
   ESPhttpUpdate.onProgress(update_progress);
-  DEBUG_CONNECT_PRINT("Start upload firmware......");
+  ESPhttpUpdate.onError(update_error);
+  Debug::LOG_TO_SCREEN(0,0,"Start upload firmware......");
   t_httpUpdate_return ret = ESPhttpUpdate.update(URI_GET_FIRMWARE, "1.0");
   switch (ret) {
   case HTTP_UPDATE_FAILED:
-    DEBUG_CONNECT_PRINT("HTTP_UPDATE_FAILD Error (%d): %s");
+    Debug::LOG_TO_SCREEN(0,0,"HTTP_UPDATE_FAILD Error");
     break;
   case HTTP_UPDATE_NO_UPDATES:
-    DEBUG_CONNECT_PRINT("HTTP_UPDATE_NO_UPDATES");
+    Debug::LOG_TO_SCREEN(0,0,"HTTP_UPDATE_NO_UPDATES");
     break;
   case HTTP_UPDATE_OK:
-    DEBUG_CONNECT_PRINT("HTTP_UPDATE_OK");
+    Debug::LOG_TO_SCREEN(0,0,"HTTP_UPDATE_OK");
     break;
   }
 }
